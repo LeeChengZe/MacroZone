7003ee3530ec9121d731f17593b9760d0762ff92
LeeChengZe <72968326+LeeChengZe@users.noreply.github.com>
2026-07-11 02:20:30 +0800

    Macro grid and storage

--- MacroZone/src/app/(tabs)/index.tsx
+++ MacroZone/src/app/(tabs)/index.tsx
@@ -1,16 +1,33 @@
+import HomeHeader from '@/components/HomeHeader';
+import MacroGrid from '@/components/MacroGrid';
+import RecentMeals from '@/components/RecentMeals';
+import { getMeals, Meal } from '@/storage/meals';
 import { globalStyles } from '@/styles/global';
-import { Link } from 'expo-router';
-import { Text, ScrollView } from 'react-native';
-import HomeHeader from '../../components/HomeHeader';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { ScrollView, Text } from 'react-native';
 
 export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log('Loaded meals:', data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
-     <HomeHeader />
-      <Link href='/meals' style={{ fontSize: 18, color: '#007bff' }}>
-        Go to Meals
-      </Link>
+      <HomeHeader />
+      <MacroGrid />
+      <RecentMeals meals={meals} />
     </ScrollView>
   );
 }
